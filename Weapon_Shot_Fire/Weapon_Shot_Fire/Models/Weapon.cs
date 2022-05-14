0cd18cdca2138043f19330d7fca3d0518b4d38ab
Yusif Nazarbayov (CodeAcademy) <99797435+yusifny@users.noreply.github.com>
2022-05-14 13:41:25 +0400

    must be done

---
+++ Weapon_Shot_Fire/Weapon_Shot_Fire/Models/Weapon.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weapon_Shot_Fire.Models
+{
+    class Weapon
+    {
+        private int _bulletCapacity;
+        private int _currentBullet;
+        public int BulletCapacity
+        {
+            get => _bulletCapacity;
+            set
+            {
+                if (value >= 0 && value <= 30)
+                {
+                    _bulletCapacity = value;
+                }
+            }
+        }
+        public int CurrentBullet
+        {
+            get => _currentBullet;
+            set
+            {
+                if (value >= 0 && value <= 30)
+                {
+                    _currentBullet = value;
+                }
+            }
+        }
+        public bool IsManual { get; set; }
+
+        public Weapon(int bulletCapacity, int currentBullet, bool isManual)
+        {
+            BulletCapacity = bulletCapacity;
+            CurrentBullet = currentBullet;
+            IsManual = isManual;
+        }
+
+        public void Shoot()
+        {
+            Console.WriteLine("Shoot method used.");
+            if (CurrentBullet > 0)
+            {
+                CurrentBullet--;
+                if (CurrentBullet > 1)
+                {
+                    Console.WriteLine($"{CurrentBullet} bullets remain.");
+                }
+                else
+                {
+                    Console.WriteLine($"{CurrentBullet} bullet remains.");
+                }
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Have no enough bullet!");
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+        }
+
+        public void Fire()
+        {
+            Console.WriteLine("Fire method used.");
+            if (CurrentBullet > 0)
+            {
+                if (IsManual == true)
+                {
+                    CurrentBullet--;
+                    if (CurrentBullet > 1)
+                    {
+                        Console.WriteLine($"{CurrentBullet} bullets remain.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{CurrentBullet} bullet remains.");
+                    }
+                }
+                else
+                {
+                    CurrentBullet = 0;
+                    Console.WriteLine($"{CurrentBullet} bullet remains.");
+                }
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Have no enough bullet!");
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+        }
+
+        public void GetRemainBulletCount()
+        {
+            Console.WriteLine($"{BulletCapacity - CurrentBullet}");
+        }
+
+        public void ChangeFireMode()
+        {
+            if (IsManual == true)
+            {
+                Console.WriteLine($"Mode is changed to Automatic");
+            }
+            else if(IsManual == false)
+            {
+                Console.WriteLine($"Mode is changed to Manual");
+            }
+
+        }
+        
+        public void Reload()
+        {
+            CurrentBullet = 30;
+        }
+    }
+}
---
+++ Weapon_Shot_Fire/Weapon_Shot_Fire/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Weapon_Shot_Fire.Models;
+
+namespace Weapon_Shot_Fire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletCapacity;
+            int currentBullet;
+            int isManual;
+            do
+            {
+                bulletCapacity = Convert.ToInt32(Console.ReadLine());
+            } while (bulletCapacity < 0);
+            do
+            {
+                currentBullet = Convert.ToInt32(Console.ReadLine());
+            } while (currentBullet < 0||currentBullet>bulletCapacity);
+
+            Weapon weapon = new Weapon(bulletCapacity, currentBullet, isManual);
+
+            
+
+            bool Check = true;
+
+            do
+            {
+                int Choice = Convert.ToInt32(Console.ReadLine());
+                switch (Choice)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    default:
+                        break;
+                }
+            } while (Check);
+
+        }
+    }
+}
